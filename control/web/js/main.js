f74590ec50a3419586b6398ebf7311c9a2d8311d
NailaValeria38800 <vale38800@gmail.com>
2024-05-14 12:41:17 -0600

    Add files via upload

---
+++ control/web/js/main.js
@@ -0,0 +1,45 @@
+//alert("Hola mundo");
+let adelante =document.getElementById("adelante");
+let atrás =document.getElementById("atrás");
+let izquierda =document.getElementById("izquierda");
+let derecha =document.getElementById("derecha");
+let detener =document.getElementById("detener");
+
+let mensaje=document.getElementById("mensaje");
+
+function callApi(estatus) {
+  // Hacer una petición para un usuario con ID especifico
+  axios
+    .get(
+      "http://localhost/iot-car-control/back-end/apis/setRegistro.php?valorEstatus=" +
+        estatus
+    )
+    .then(function (response) {
+      // manejar respuesta exitosa
+      console.log(response);
+      mensaje.innerHTML="Respuesta: <stromp>" + response.data + "</stromp>";
+    })
+    .catch(function (error) {
+      // manejar error
+      console.log(error);
+    })
+    .finally(function () {
+      // siempre sera executado
+    });
+}
+
+adelante.addEventListener("click", function () {
+    callApi('f');
+});
+atrás.addEventListener("click", function () {
+  callApi('b');
+});
+izquierda.addEventListener("click", function () {
+  callApi('l');
+});
+derecha.addEventListener("click", function () {
+  callApi('r');
+});
+detener.addEventListener("click", function () {
+  callApi('s');
+});
